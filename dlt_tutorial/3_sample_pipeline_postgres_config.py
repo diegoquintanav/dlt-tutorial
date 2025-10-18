6cffc6354b10b0d3c67545e8030aa0009cf41e55
Diego Quintana <daquintanav@gmail.com>
2025-10-18 15:19:41 +0200

    ref: update script 3 with fixed data

--- dlt_tutorial/3_sample_pipeline_postgres_config.py
+++ dlt_tutorial/3_sample_pipeline_postgres_config.py
@@ -1,18 +1,38 @@
-import string
+import datetime as dt
+from pathlib import Path
 from typing import Generator
-from uuid import uuid4
 
 import dlt
 
 
 @dlt.resource
 def sample_data() -> Generator[dict, None, None]:
-    for x in range(2):
-        yield {
-            "id": x,
-            "name": "Mr. " + string.ascii_letters[x],
-            "random_field": uuid4(),
-        }
+    my_data = [
+        {
+            "id": 1,
+            "name": "Mr. Mario",
+            "uuid": "a6d7b6dd-bcdb-422e-83eb-f53b2eb4f2cc",
+            "created_at": "2025-10-09 14:40:00",
+            "updated_at": "2025-10-09 14:50:00",
+            "metadata": {
+                "ingested_at": dt.datetime.now().isoformat(),
+                "script_name": Path(__file__).name,
+            },
+        },
+        {
+            "id": 2,
+            "name": "Mr. Luigi",
+            "uuid": "8c804ede-f8ae-409e-964d-9e355a3094e0",
+            "created_at": "2025-10-08 16:15:00",
+            "updated_at": "2025-10-08 16:50:00",
+            "metadata": {
+                "ingested_at": dt.datetime.now().isoformat(),
+                "script_name": Path(__file__).name,
+            },
+        },
+    ]
+    for item in my_data:
+        yield item
 
 
 @dlt.source
@@ -31,6 +51,7 @@ if __name__ == "__main__":
     load_info = pipeline.run(
         sample_source,
         table_name="samples",
+        refresh="drop_sources",
         write_disposition={
             "disposition": "replace",
         },
